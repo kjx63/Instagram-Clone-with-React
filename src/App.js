9c7d4cda6ff33eb126e1af51f482f9d188c88fc0
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-19 13:55:19 +0900

    Firebase User Authentication with Modal popu to sign Up/In and Logout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './components/Post';
-import { db } from './firebase';
+import { db, auth } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Input, Modal } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out...
+        setUser(null);
+      }
+    });
+
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    };
+  }, [user, username]);
 
   useEffect(() => {
     db.collection('posts').onSnapshot((snapshot) => {
@@ -12,8 +63,97 @@ function App() {
     });
   }, []);
 
+  const signUp = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((err) => alert(err.message));
+
+    setOpen(false);
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => alert(err.message));
+
+    setOpenSignIn(false);
+  };
+
   return (
     <div className='app'>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img
+                className='app__headerImage'
+                src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+                alt='https://www.extremetech.com/wp-content/uploads/2019/09/SubForester-764A6822edit.jpg'
+              />
+            </center>
+            <Input
+              type='text'
+              placeholder='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type='email'
+              placeholder='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button type='submit' onClick={signUp}>
+              Sign Up
+            </Button>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img
+                className='app__headerImage'
+                src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+                alt='https://www.extremetech.com/wp-content/uploads/2019/09/SubForester-764A6822edit.jpg'
+              />
+            </center>
+            <Input
+              type='email'
+              placeholder='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type='password'
+              placeholder='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button type='submit' onClick={signIn}>
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Modal>
       <div className='app__header'>
         <img
           className='app__headerImage'
@@ -21,9 +161,19 @@ function App() {
           alt='https://www.extremetech.com/wp-content/uploads/2019/09/SubForester-764A6822edit.jpg'
         />
       </div>
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ) : (
+        <div className='app__loginContainer'>
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        </div>
+      )}
+
       <h1>Let's build an Instagram Clone with React🚀❗️</h1>
       {posts.map(({ post, id }) => (
         <Post
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
